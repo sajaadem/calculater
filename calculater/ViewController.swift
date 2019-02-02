05bd591f2a157f0f28029264a86ac8fbb9c4fd29
Saja <saja@sajas-macbook-air.local>
2019-02-02 22:21:15 +0200

    Initial Commit

---
+++ calculater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculater
+//
+//  Created by Saja on 2/2/19.
+//  Copyright © 2019 Saja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
